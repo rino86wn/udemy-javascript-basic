ae9e338bd6f7bc8c22bd6cf017e5da7f90e12247
Rino Takase <70308286+rino86wn@users.noreply.github.com>
2022-05-13 01:44:23 +0900

    initial commit

---
+++ src/section4/19.js
@@ -0,0 +1,15 @@
+/**
+ * テンプレート文字列
+ */
+// const name = "梨乃";
+// const age = 24;
+
+// // //「私の名前は梨乃です。年齢は24歳です。」を表示する
+
+// // //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// // //テンプレート文字列
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
